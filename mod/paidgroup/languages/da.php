f3d52d0cd99982927fa24a0be665e21df75965a2
TheMuller <khm@kemster.com>
2014-02-27 21:07:28 +0100

    Change  DA language file to UTF without BOM

--- mod/paidgroup/languages/da.php
+++ mod/paidgroup/languages/da.php
@@ -11,45 +11,46 @@ $danish = array(
                  'groups_currency' => "Valuta",
                  'groups_merchant_number' => " DIBS merchant number",
                  'groups_md5secret' => "DIBS md5 secret",
-                 'paidgroup:membership:lastdate' => "Dit medlemsskab udløber den %s",
+                 'paidgroup:membership:lastdate' => "Dit medlemsskab udlÃ¸ber den %s",
                  'paidgroup:membership:warning' =>  "Dette er en betalingsgruppe",
-                 'paidgroup:membership:expired:warning'=> "Dit medlemsskab er udløbet",
-                 'paidgroup:membership:leave:confirm' => "Er du sikker på, at du vil forlade gruppen? Du skal betale for et nyt medlemsskab, hvis du ønsker at blive gruppemedlem igen",
+                 'paidgroup:membership:expired:warning'=> "Dit medlemsskab er udlÃ¸bet",
+                 'paidgroup:membership:leave:confirm' => "Er du sikker pÃ¥, at du vil forlade gruppen? Du skal betale for et nyt medlemsskab, hvis du Ã¸nsker at blive gruppemedlem igen",
 
-                 'paidgroup:field:group_paid_flag'=> "Skal det være et betalt medlemsskab?",
+                 'paidgroup:field:group_paid_flag'=> "Skal det vÃ¦re et betalt medlemsskab?",
                  'paidgroup:field:group_period_type'=> "Periode type",
-                 'paidgroup:field:group_paid_LockedPeriod'=> "Medlemsskabs perioden i måneder",
+                 'paidgroup:field:group_paid_LockedPeriod'=> "Medlemsskabs perioden i mÃ¥neder",
                  'paidgroup:field:group_paid_MembershipStart'=> "Medlemsskabets start dato",
                  'paidgroup:field:group_paid_MembershipEnd'=> "Medlemsskabets slut dato",
                  'paidgroup:field:group_paid_price'=> "Pris for medlemsskabet",
 
-                 'paidgroup:field:group_period_type:value:duration'=> "Månedsperiode",
+                 'paidgroup:field:group_period_type:value:duration'=> "MÃ¥nedsperiode",
                  'paidgroup:field:group_period_type:value:dates'=> "Datoer",
                  'paidgroup:field:group_price_type:value:dailyprice'=> "Dags pris",
                  'paidgroup:field:group_price_type:value:fixedprice'=> "Fast pris",
-                 'paidgroup:field:warning_activemember'=> "Der er %s aktive medlemmer. En ændring i prisen vil ikke påvirke dem. Men ændringer i datoer vil send en information til medlemmerne via email.",
+                 'paidgroup:field:warning_activemember'=> "Der er %s aktive medlemmer. En Ã¦ndring i prisen vil ikke pÃ¥virke dem. Men Ã¦ndringer i datoer vil send en information til medlemmerne via email.",
 
 //Email : membership will expire
-                 'paidgroup:membership:will:expire:email:subject'=> "Dit medlemsskab udløber snart",
+                 'paidgroup:membership:will:expire:email:subject'=> "Dit medlemsskab udlÃ¸ber snart",
                  'paidgroup:lock:membership:will:expire:email:body' => "Hej %s,
-Dit medlemsskab for gruppen:%s udløber den %s.",
+Vi vil gerne gÃ¸re dig opmÃ¦rksom pÃ¥, at dit medlemsskab for gruppen:%s udlÃ¸ber den %s.
+Du kan forlÃ¦nge dit abonnement ved at logge pÃ¥, sÃ¥ snart dit abonnement er udlÃ¸bet.",
                  'paidgroup:date:membership:will:expire:email:body' => "Hej %s,
-Dit medlemsskab for gruppen: %s kommer til at udløbe den %s.",
+Dit medlemsskab for gruppen: %s kommer til at udlÃ¸be den %s.",
 //Email :  membership is expired
-                 'paidgroup:membership:expired:email:subject'=> "Dit medlemsskab er nu udløbet..",
+                 'paidgroup:membership:expired:email:subject'=> "Dit medlemsskab er nu udlÃ¸bet..",
                  'paidgroup:membership:expired:email:body' => "Hej %s,
-Dit medlemsskab for gruppen: %s udløber i dag.",
+Dit medlemsskab for gruppen: %s udlÃ¸ber i dag.",
 //Email:  membership is expired                 
-                 'paidgroup:datechanged:email:subject'=> "Dato for dit medlemsskab er ændret",
+                 'paidgroup:datechanged:email:subject'=> "Dato for dit medlemsskab er Ã¦ndret",
                  'paidgroup:datechanged:email:body' => "Hej %s,
-Der er har været en ændring i datoopsætningen for gruppen: %s.
+Der er har vÃ¦ret en Ã¦ndring i datoopsÃ¦tningen for gruppen: %s.
 Nu er Start datoen sat til den %s, og Slut datoen til den %s.",
 
 //Email:  Payment Received
                  'paidgroup:invoice:email:subject'=> "Modtaget din betaling",
                  'paidgroup:invoice:email:body' => "Hej %s,
 Vi har modtaget din betaling for gruppen: %s.
-Tak for din tilmelding. God fornøjelse med medlemsskabet.",
+Tak for din tilmelding. God fornÃ¸jelse med medlemsskabet.",
                  
 );
 
