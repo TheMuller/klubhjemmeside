32e631f03b5bb046091108241f6380a79ba7d983
enRaiser <sachin@enraiser.com>
2014-03-07 16:48:23 +0530

    Gallery Tooltip created

--- mod/paidgroup/views/default/paidgroup/join_payment_handler_block.php
+++ mod/paidgroup/views/default/paidgroup/join_payment_handler_block.php
@@ -79,6 +79,68 @@
 ?>
 </div>
 <style>
+/* tooltip basic link styles */
+  .tooltip:link,
+  .tooltip:visited {
+    position:relative;
+    text-decoration:underline;
+  }
+  .tooltip:hover {
+    text-decoration:none; /* remove underline on tooltip text */
+  }
+
+  /* tooltip body text */
+  .tooltip:hover:before {
+    display:block;
+    background:#eee;
+    background:-webkit-gradient(linear, 0 0, 0 100%, from(rgba(255,205,205,0.9)), to(rgba(228,230,230,0.9)));
+    background:-moz-linear-gradient(rgba(255,205,205,0.9), rgba(228,230,230,0.9));
+    background:-o-linear-gradient(rgba(255,205,205,0.9), rgba(228,230,230,0.9));
+    background:linear-gradient(rgba(255,205,205,0.9), rgba(228,230,230,0.9));
+    content:attr(data-tooltip); /* this link attribute contains tooltip text */
+    position:absolute;
+    font-size:0.9em;
+    color:rgba(51,51,51,0.9);
+    bottom:20px;/* ensure link text is visible under tooltip */
+    right:0px;  /* align both tooltip and link right edges */
+    width:11em;  /* a reasonable width to wrap tooltip text */
+    text-align:center;
+    padding:4px;
+    border:2px solid rgba(204,153,153,0.9);
+    -webkit-border-radius:6px;
+       -moz-border-radius:6px;
+        -ms-border-radius:6px;
+         -o-border-radius:6px;
+            border-radius:6px;
+    -webkit-box-shadow:-2px -2px 2px rgba(20,20,20,0.4);
+       -moz-box-shadow:-2px -2px 2px rgba(20,20,20,0.4);
+        -ms-box-shadow:-2px -2px 2px rgba(20,20,20,0.4);
+         -o-box-shadow:-2px -2px 2px rgba(20,20,20,0.4);
+            box-shadow:-2px -2px 2px rgba(20,20,20,0.4);
+  }
+
+  /* styles shared by both triangles */
+  .tooltip:hover span:before,
+  .tooltip:hover span:after {
+    content:"";
+    position:absolute;
+    border-style:solid;
+  }
+  /* outer triangle: for border */
+  .tooltip:hover span:before {
+    bottom:5px; /* value = tooltip:hover:before (border-width*2)+1 */
+    right:10px; /* controls horizontal position */
+    border-width:16px 16px 0; /* top, right-left, bottom */
+    border-color:rgba(204,153,153,0.9) transparent; /* top/bottom, right-left (lazy becasue bottom is 0) */
+  }
+
+  /* inner triangle: for fill */
+  .tooltip:hover span:after {
+    bottom:8px; /* value = tooltip:before (border-width*2) */
+    right:12px; /* above 'right' value + 2 */
+    border-width:14px 14px 0; /* 2 less than above */
+    border-color:rgba(225,238,238,0.95) transparent; /* tweak opacity by eye/eyedropper to obscure outer triangle colour */
+  }
 .group-gallery-item  {
     float: left;
 background:white   ;
