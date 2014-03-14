5437a94ff1ce788b39c8bee4f12e4a0892ce3950
enRaiser <sachin@enraiser.com>
2014-03-14 17:42:40 +0530

    member extend nav menu

--- mod/members_extend/views/default/members/nav.php
+++ mod/members_extend/views/default/members/nav.php
@@ -2,7 +2,8 @@
 /**
  * Members navigation
  */
- 
+ 	$myurl= elgg_get_site_url()."action/member/download";
+	$myurl = elgg_add_action_tokens_to_url($myurl);
 
 $tabs = array(
 	'newest' => array(
@@ -20,26 +21,44 @@ $tabs = array(
 		'href' => "members/online",
 		'selected' => $vars['selected'] == 'online',
 	),
-'xlupload' => array(
-            'text' => elgg_echo('XL Upload'),
-            'href' => "members/upload",
-            'priority' =>1000
-            ),
-'xldownload' => array(
-            'text' => elgg_echo('XL Download'),
-            'href' => "members/download",
-            'priority' =>1000
-            ),
 	
+
 );
 $user= get_entity($_SESSION['user']->guid);
 if($user){
 	if($user->isAdmin()){
-		$tabs['unvalidated'] =  array(
+		$tabs = array(
+		'newest' => array(
+		'text' => elgg_echo('members:label:newest'),
+		'href' => "members/newest",
+		'selected' => $vars['selected'] == 'newest',
+	),
+	'popular' => array(
+		'text' => elgg_echo('members:label:popular'),
+		'href' => "members/popular",
+		'selected' => $vars['selected'] == 'popular',
+	),
+	'online' => array(
+		'text' => elgg_echo('members:label:online'),
+		'href' => "members/online",
+		'selected' => $vars['selected'] == 'online',
+	),
+	'unvalidated' =>  array(
 				'text' => elgg_echo('admin:users:unvalidated'),
 				'href' => "members/unvalidated",
 				'selected' => $vars['selected'] == 'unvalidated',
-					);
+	),
+	'xlupload' => array(
+            'text' => elgg_echo('XL Upload'),
+            'href' => "members/upload",
+            'priority' =>1000
+    ),
+	'xldownload' => array(
+            'text' => elgg_echo('XL Download'),
+            'href' => $myurl,
+            'priority' =>1000
+    ),
+		);
 
 	}
 }
