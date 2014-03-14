d5f192b220eb30edf3b2da8997b8e74b8b9e4001
enRaiser <sachin@enraiser.com>
2014-03-14 11:57:48 +0530

    minor

--- mod/PHPExcel/views/default/admin/PHPExcel/settings.php
+++ mod/PHPExcel/views/default/admin/PHPExcel/settings.php
@@ -1,5 +1,7 @@
 
 <?php
+	//global $_SESSION;
+	$group_guid =  get_input('group_guid','');
     $step = get_input('step','1');
     
     if($step =='1'){
@@ -24,10 +26,28 @@
         
         
     }elseif($step =='3'){
-        echo "sucesss...";
+	
+        foreach($_SESSION['not_member'] as $Data)
+		{
+			$user = get_user_by_username($Data);
+			join_group($group_guid,$user->guid);
+			//echo $Data;
+		}
+		echo "<b>".$Data."</b>"." Following Persone are Succesfully Joined The Group ";
+		$groups = elgg_get_entities(array('type'=>'group',));
+			foreach($groups as $group)
+			{
+				$grouparr[$group->guid]=$group->name;
+				if($group->guid == $group_guid)
+				echo  "<b>".$group->name."</b>";
+			}
+			echo " ...";
+		
+		
+		
     }
     else{
-        $group_guid =  get_input('group_guid','');
+
         //echo $group_guid;
         $group = get_entity($group_guid);
         //$user_guid= elgg_get_logged_in_user_entity();
@@ -57,48 +77,40 @@
             
             $inputFileName = './'.$newName;  // File to read
             
-            try {
+            try 
+			{
                 $objPHPExcel = PHPExcel_IOFactory::load($target);
-            } catch(Exception $e) {
+            }
+			catch(Exception $e) 
+			{
                 die('Error loading file "'.pathinfo($target,PATHINFO_BASENAME).'": '.$e->getMessage());
             }
-            
-            
             //echo '<hr />';
-            
             $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
             //echo "afetr..";
             //print_r($sheetData);
-            
             foreach ($sheetData as $data)
             {
-                
                 $user = get_user_by_username($data[A]);
                 //echo $user->username;
-                
                 if ($user instanceof ElggUser)
                 {
                     //echo "User Exist...";
-                    
-                    if(is_group_member($group_guid,$user->guid)){
-                        
+                    if(is_group_member($group_guid,$user->guid))
+					{
                         //echo "yes";
-                        
                     }
                     else
                     {
-                        //echo "<td>";
-                        //echo "no";
-                        //echo "</td>";
-                        
+                        //echo "<td>";//echo "no";//echo "</td>";
+						
                         echo elgg_view_entity($user);
-                        join_group($group_guid,$user->guid);
-                    }
+                        //join_group($group_guid,$user->guid);
+						$_SESSION['not_member'][]=$data[A];
+					}
                 }
                 else
-                {
-                    
-                    
+                {                   
                     $not_users[]=$data[A];
                 }
                 
@@ -117,11 +129,13 @@
         }
         
         echo elgg_view("output/url",
-                       array('href' => elgg_get_site_url().'admin/PHPExcel/settings?step=3','text' => elgg_echo('submit'),
+                       array('href' => elgg_get_site_url().'admin/PHPExcel/settings?step=3&group_guid='.$group_guid,'text' => elgg_echo('submit'),
                              'class' => 'elgg-button elgg-button-delete',));
-        echo "<hr>";
+        echo "<hr width='20%' align='left'>";
+		echo "<b>Not Joined User's</b>";echo "<hr width='20%' align='left'>";
+		//$objPHPExcel->getActiveSheet()->removeRow(1,1);  Remove First line excel
         foreach ($not_users as $not_user)
-        {
+        {	
             echo $not_user;
             echo "<br>";
         }
