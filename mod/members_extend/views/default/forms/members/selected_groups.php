9f14afda4237c157b37adc57c7366a4f3787d5d4
enRaiser <sachin@enraiser.com>
2014-04-28 12:04:44 +0530

    checkbox in scrollbar

--- mod/members_extend/views/default/forms/members/selected_groups.php
+++ mod/members_extend/views/default/forms/members/selected_groups.php
@@ -9,13 +9,14 @@ $groups = elgg_get_entities($options);
 foreach($groups as $group){
 	$myarr[$group->name] = $group->guid;
 }
-
+echo "<div style='padding: 5px;overflow: scroll;overflow-x: scroll;overflow-y: hidden;width: 960px;max-height: 30px;white-space: nowrap;'>";
 echo elgg_view('input/checkboxes', array(
             'options' =>$myarr,   
             'align' => 'horizontal',
 			'value' => $_SESSION['member_extend_selected_groups'],
 			'name' => 'selected_groups',
         ));
+		echo "</div>";
 echo elgg_view('input/text',
 	array(
 		'name'=> 'searchquery',
