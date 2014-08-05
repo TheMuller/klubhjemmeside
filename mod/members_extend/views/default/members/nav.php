0fbbdb8ebf6f7f03e46a1eeb7ef504f7d47d4de1
enRaiser <sachin@enraiser.com>
2014-08-05 15:23:42 +0530

    bug fix #95 (validatebyadmin)

--- mod/members_extend/views/default/members/nav.php
+++ mod/members_extend/views/default/members/nav.php
@@ -173,42 +173,54 @@ $(document).ready(function() {
 });
 </script>
 <?php
-		$tabs = array(
-		'newest' => array(
-		'text' => elgg_echo('members:label:newest'),
-		'href' => "members/newest",
-		'selected' => $vars['selected'] == 'newest',
-	),
-	'popular' => array(
-		'text' => elgg_echo('members:label:popular'),
-		'href' => "members/popular",
-		'selected' => $vars['selected'] == 'popular',
-	),
-	'online' => array(
-		'text' => elgg_echo('members:label:online'),
-		'href' => "members/online",
-		'selected' => $vars['selected'] == 'online',
-	),
-	'unvalidated' =>  array(
-				'text' => elgg_echo('admin:users:unvalidated'),
-				'href' => "members/unvalidated",
-				'selected' => $vars['selected'] == 'unvalidated',
-	),
-	'xlupload' => array(
-            'text' => elgg_echo('members:xl_upload'),
-            'href' => "members/upload",
-            'priority' =>1000
-    ),
-	'xldownload' => array(
-            'text' => elgg_echo('members:xl_download'),
-            'href' => $myurl,
-            'priority' =>1000
-    ),
+		$uservalidationbyadmin_plugin = elgg_get_plugin_from_id('uservalidationbyadmin');
+		$tab1 = array(
+			'newest' => array(
+				'text' => elgg_echo('members:label:newest'),
+				'href' => "members/newest",
+				'selected' => $vars['selected'] == 'newest',
+			),
+			'popular' => array(
+				'text' => elgg_echo('members:label:popular'),
+				'href' => "members/popular",
+				'selected' => $vars['selected'] == 'popular',
+			),
+			'online' => array(
+				'text' => elgg_echo('members:label:online'),
+				'href' => "members/online",
+				'selected' => $vars['selected'] == 'online',
+			),
 		);
+
+		$tab2 = array(
+			'unvalidated' =>  array(
+						'text' => elgg_echo('admin:users:unvalidated'),
+						'href' => "members/unvalidated",
+						'selected' => $vars['selected'] == 'unvalidated',
+			),
+		);
+
+		$tab3 = array(
+			'xlupload' => array(
+					'text' => elgg_echo('members:xl_upload'),
+					'href' => "members/upload",
+					'priority' =>1000
+			),
+			'xldownload' => array(
+					'text' => elgg_echo('members:xl_download'),
+					'href' => $myurl,
+					'priority' =>1000
+			),
+		);
+	
+		if($uservalidationbyadmin_plugin->IsActive()){
+			$tabs = array_merge($tab1,$tab2,$tab3);
+		}else{
+			$tabs = array_merge($tab1,$tab3);
+		}
 
 	}
 }
-    
 foreach($tabs as $name => $tab){
     $tab["name"] = $name;
     elgg_register_menu_item("projecttabs", $tab);
